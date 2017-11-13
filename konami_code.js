8dcf3097ef2de3b271f2cc463d5fea7d73c04842
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-13 18:08:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   const input = document.body
-  input.addEventListener('keydown', function (e){
+  input.addEventListener('keydown', onKeyDownHandler{
 
   })
 }
