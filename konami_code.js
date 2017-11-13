b183492cf1234e44b2479804c92fb0d57f27e5b0
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-13 18:09:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,7 @@ let index = 0;
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
-
+  console.log("!!!!", key)
   if (key === code[index]) {
     index++;
 
